f48424ae9c249c133745edad03ac469e1a5d358d
Marcy Bursac <msbursac@gmail.com>
2018-09-03 21:36:58 -0500

    adds serviceWorker

---
+++ sw.js
@@ -0,0 +1,27 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('restaurant-static-v1').then(function(cache) {
+            return cacheAll(
+                '/',
+                'js/main.js',
+                'js/restaurant_info.js',
+                'css/styles.css',
+                'css/responsive_index_styles.css',
+                'css/responsive_restaurant_styles.css',
+                'img/',
+                'restaurant.html?'
+            );
+        })
+
+    );
+});
+
+
+self.addEventListener('fetch', function() {
+    event.respondWith(
+        catches.match(event.request).then(function(response) {
+            if(response) return response;
+            return fetch(event.request);
+        })
+    );
+});
