257f3faa15f90b90c42fd2d72d160b44124dbf6c
Marcy Bursac <msbursac@gmail.com>
2018-09-07 21:48:11 -0500

    add mapbox token to restaurant page, adjusts styles

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoibWFyY3liIiwiYSI6ImNqbGZ2OHR3bzBoeWYza3JrbDI1c3lpMWUifQ.b9s_J7mObDCETD_0SecVpg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
