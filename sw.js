0fd62d07d784887a349c5976cc9b01880034d46c
Marcy Bursac <msbursac@gmail.com>
2018-09-09 08:49:01 -0500

    adds event listener to open cache

--- sw.js
+++ sw.js
@@ -1,52 +1,54 @@
-// service worker as learned at https://www.sitepoint.com/getting-started-with-service-workers/
+// Service Worker fetching per https://developers.google.com/web/fundamentals/primers/service-workers/
 
+var CACHE_VERSION = 'app-v1';
+var CACHE_FILES = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    'css/styles.css',
+    'css/responsive_index_styles.css',
+    'css/responsive_restaurant_styles.css',
+    'data/restaurants.json',
+    '/img/',
+    'js/dbhelper.js',
+    'js/main.js',
+    'js/restaurant_info.js',
+    '/sw.js'
+];
 
-
-// var CACHE_VERSION = 'app-v1';
-// var CACHE_FILES = [
-//     '/',
-//     'img',
-//     'js/dbhelper.js',
-//     'js/main.js',
-//     'js/restaurant_info.js',
-//     'css/styles.css',
-//     'css/responsive_index_styles.css',
-//     'css/responsive_restaurant_styles.css',
-// ];
-
-// // installs the Service Worker
-// self.addEventListener('install', function (event) {
-//     event.waitUntil(
-//         caches.open(CACHE_VERSION)
-//             .then(function (cache) {
-//                 console.log('Opened cache');
-//                 return cache.addAll(CACHE_FILES);
-//             })
-//     );
-// });
-
-
-// fetches requests made to the server
- self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request).then(function(res){
-            if(res){
-                return res;
-            }
-            requestBackend(event);
-        })
-    )
+// installs the Service Worker
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_VERSION)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(CACHE_FILES);
+            })
+    );
 });
+
+
+// // fetches requests made to the server
+//  self.addEventListener('fetch', function (event) {
+//     event.respondWith(
+//         caches.match(event.request).then(function(response){
+//             if(response){
+//                 return response;
+//             }
+//             requestBackend(event);
+//         })
+//     )
+// });
 
 // function requestBackend(event){
 //     var url = event.request.clone();
-//     return fetch(url).then(function(res){
+//     return fetch(url).then(function(response){
 //         //if not a valid response send the error
-//         if(!res || res.status !== 200 || res.type !== 'basic'){
+//         if(!response || res.status !== 200 || response.type !== 'basic'){
 //             return res;
 //         }
 
-//         var response = res.clone();
+//         var response = response.clone();
 
 //         caches.open(CACHE_VERSION).then(function(cache){
 //             cache.put(event.request, response);
